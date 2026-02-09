87b6f241a5dd398ed8aebfac1a25082b09253477
levish0 <shiueo.csh@gmail.com>
2026-02-10 00:46:57 +0900

    remove

--- mofumofu-server/crates/mofumofu-worker/src/jobs/mod.rs
+++ mofumofu-server/crates/mofumofu-worker/src/jobs/mod.rs
@@ -2,16 +2,14 @@ pub mod cron;
 pub mod email;
 pub mod index;
 pub mod reindex;
-pub mod storage;
 
 // Re-exports for backward compatibility with mofumofu_server
-pub use index::post as post_index;
 pub use index::user as user_index;
 
 use crate::config::WorkerConfig;
 use crate::connection::R2Client;
 use crate::nats::JetStreamContext;
-use crate::{CacheClient, DbPool, Mailer, SearchClient, StorageClient};
+use crate::{CacheClient, DbPool, Mailer, SearchClient};
 
 /// Shared context for worker registration
 #[derive(Clone)]
@@ -20,7 +18,6 @@ pub struct WorkerContext {
     pub meili_client: SearchClient,
     pub db_pool: DbPool,
     pub cache_client: CacheClient,
-    pub storage_client: StorageClient,
     pub r2_client: R2Client,
     pub jetstream: JetStreamContext,
     pub config: &'static WorkerConfig,
